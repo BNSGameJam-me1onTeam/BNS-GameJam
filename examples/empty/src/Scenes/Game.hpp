c91c63cba80ebf6f29a47ec7464d1d559bb124a5
melon0658 <meronn0818@gmail.com>
2023-10-26 03:02:08 +0900

    privateメンバの追加
    ゲームの状態を記録するクラスの追加
    画像表示のためのメンバ変数追加
    画像位置設定用のメンバ変数追加
    ストップウォッチ用のメンバ変数追加
    カウントダウン用のメンバ変数追加

--- examples/empty/src/Scenes/Game.hpp
+++ examples/empty/src/Scenes/Game.hpp
@@ -12,6 +12,25 @@ public:
     void draw() const override;
 
 private:
+    enum class GameState
+    {
+        Countdown,
+        StartPlaying,
+        Playing,
+        Finished
+    };
+    
     Texture m_texture;
+    Texture m_texture_guzai;
+    Texture m_texture_otama;
+    
+    GameState m_state;
+    
+    Stopwatch m_stopwatch;
+    
+    int m_countdownSeconds;
+    
+    Vec2 m_position_guzai;
+    Vec2 m_position_otama;
 };
 
