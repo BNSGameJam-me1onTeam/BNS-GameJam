75ae78d2b531b773f6263ba134a3fd4555796772
nakazi <nakazi1209@gmail.com>
2023-10-30 00:27:17 +0900

    ステージセレクトシーンに1ステージ分の画像を挿入

--- examples/empty/src/Scenes/SelectStage.cpp
+++ examples/empty/src/Scenes/SelectStage.cpp
@@ -2,7 +2,7 @@
 #include "SelectStage.hpp"
 #include "Controller.hpp"
 
-SelectStage::SelectStage(const InitData& init) : IScene{ init }, m_texture{ U"🐈"_emoji }
+SelectStage::SelectStage(const InitData& init) : IScene{ init }, textureStageOden(U"example/StageOden.png")
 {
     Print << U"SelectStage::SelectStage()";
 }
@@ -24,7 +24,7 @@ void SelectStage::update()
         getData().stage_id += 1;
         // Print << getData().stage_id;
     }
-    if (KeyQ.down())
+    if (getData().p1_input.Confirm.down() || getData().p2_input.Confirm.down())
     {
         changeScene(State::Title);
     }
@@ -36,13 +36,12 @@ void SelectStage::update()
 
 void SelectStage::draw() const
 {
-    RoundRect{ 140, 210, 300, 300, 10 }.draw(Palette::Seagreen);
-    RoundRect{ 490, 210, 300, 300, 10 }.draw(Palette::Skyblue);
-    RoundRect{ 840, 210, 300, 300, 10 }.draw(Palette::Black);
+    Rect{ 490, 210, 300 }.draw(Palette::Skyblue);
+    Rect{ 840, 210, 300 }.draw(Palette::Black);
     
-    RoundRect{Arg::center(Scene::Center()+Point{cursor+350*getData().stage_id-350, 0}), 300, 300, 10}.drawFrame(0, 5, Palette::Orange);
+    Rect{Arg::center(Scene::Center()+Point{cursor+350*getData().stage_id-350, 0}), 300}.drawFrame(0, 5, Palette::Orange);
+    textureStageOden.resized(300).draw(140,210);
     Scene::SetBackground(ColorF(1, 1, 1));
-    m_texture.drawAt(Cursor::Pos());
 }
 
 
--- examples/empty/src/Scenes/SelectStage.hpp
+++ examples/empty/src/Scenes/SelectStage.hpp
@@ -12,6 +12,6 @@ public:
     void draw() const override;
 
 private:
-    Texture m_texture;
+    Texture textureStageOden;
     int cursor = 0;
 };
