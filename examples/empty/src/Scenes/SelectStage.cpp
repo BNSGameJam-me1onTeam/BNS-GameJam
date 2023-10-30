9c2205dec0d14b62c2eb9ae2a978344bf5adfdae
nakazi <nakazi1209@gmail.com>
2023-10-30 22:46:04 +0900

    タイトル追加、UIの位置を調整

--- examples/empty/src/Scenes/SelectStage.cpp
+++ examples/empty/src/Scenes/SelectStage.cpp
@@ -24,11 +24,6 @@ void SelectStage::update()
         getData().stage_id += 1;
         // Print << getData().stage_id;
     }
-    // 後々カーソル操作にするので、後でなくす。ステージをクリックすることで次のシーンに移動する
-//    if (getData().p1_input.Confirm.down() || getData().p2_input.Confirm.down())
-//    {
-//        changeScene(State::Title);
-//    }
     // 「戻る」が押されたときの処理
     if (getData().p1_input.Confirm.down() && getData().stage_id == -1)
     {
@@ -43,24 +38,24 @@ void SelectStage::draw() const
 {
     
     
-    RoundRect{ 90, 90, 150, 60 , 10}.draw(ColorF{ 0.0 });
-    textureStageOden.resized(300).draw(140,210);
-    textureStageOden.resized(300).draw(140,210);
-    textureComingSoon.resized(300).draw(490,210);
-    textureComingSoon.resized(300).draw(840,210);
+    RoundRect{ 90, 150, 250, 60 ,10}.draw(ColorF{ 0.6 });
+    textureStageOden.resized(300).draw(140,300);
+    textureStageOden.resized(300).draw(140,300);
+    textureComingSoon.resized(300).draw(490,300);
+    textureComingSoon.resized(300).draw(840,300);
     
-    FontAsset(U"TitleFont")(U"ステージを選んでね！（仮）").drawAt(Scene::Center()-Point{0, 250}, ColorF{1.0, 1.0, 1.0});
+    FontAsset(U"TitleFont")(U"ステージを選んでね！（仮）").drawAt(Scene::Center()-Point{0, 260}, ColorF{1.0, 1.0, 1.0});
     
-    FontAsset(U"LargeFont")(U"戻る").drawAt(Scene::Center()-Point{470, 240}, ColorF{1.0, 1.0, 1.0});
+    FontAsset(U"NormalFont")(U"タイトルに戻る").drawAt(Scene::Center()-Point{430, 180}, ColorF{1.0, 1.0, 1.0});
     
     if (getData().stage_id == -1)
     {
-        Rect{ 90, 90, 150, 60 }.drawFrame(0, 5, Palette::Orange);
+        Rect{ 90, 150, 250, 60 }.drawFrame(0, 5, Palette::Orange);
     }
     
     if (getData().stage_id != -1)
     {
-        Rect{Arg::center(Scene::Center()+Point{cursor+350*getData().stage_id-350, 0}), 300}.drawFrame(0, 5, Palette::Orange);
+        Rect{Arg::center(Scene::Center()+Point{cursor+350*getData().stage_id-350, 90}), 300}.drawFrame(0, 5, Palette::Orange);
     }
     Scene::SetBackground(ColorF(0.2, 0.2, 0.2));
 }
